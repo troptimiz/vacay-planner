60d083fb2ba62bb1de0115496c259bac7a53710c
mwzcommerce <raghavan@Raghavans-MacBook-Pro.local>
2015-04-11 13:04:17 +0530

    Completed Update/Delete operations for Categories

--- admin/controllers/CategoryController.js
+++ admin/controllers/CategoryController.js
@@ -1,19 +1,50 @@
 var express = require('express');
 var CategoryController = express();
 var Categories = require('../models/categories.js');
+var bodyParser = require('body-parser');
+
+CategoryController.use(bodyParser());
 
 
+// All Active Categories
 CategoryController.get('/',function(req,res){
 	Categories.find({'is_active':true},function(err,categories){
 		res.status(200).json({activeCategories:categories});
 	});	
 });
 
-
+// Category Detail using ID
 CategoryController.get('/category/:id',function(req,res){
 	Categories.findById(req.params.id,function(err,category){
 		res.status(200).json({category:category});
 	})
+});
+
+// Create new category
+CategoryController.post('/category/',function(req,res){
+	var newCategory = new Categories({
+		name:req.body.name,
+		description:req.body.description,
+		// need to see how actual image content can be uploaded ??
+		imageUrl:req.body.imageUrl,
+		is_active:req.body.isActive
+
+	});
+	newCategory.save(function(err,a){
+		if(err) return res.send(500,'Error Occured: database error');
+		res.json({'status':'Category '+a._id+' Created '});
+	});
+
+});
+
+
+// Delete Category By Id
+CategoryController.delete('/category/:id',function(req,res){
+	Categories.findById(req.params.id,function(err,category){
+		if(err)return res.send(500,'Error Occured:database error'+err);
+		category.remove();
+		res.status(200).json({'status':'Category '+req.params.id +' Deleted'});
+	});
 });
 
 
