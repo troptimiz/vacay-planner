1502278ded07170c5188f2a98f5271d524e679cd
mwzcommerce <mwzcommerce@gmail.com>
2015-05-23 17:36:04 +0530

    Modified UserController.js

--- admin/controllers/UserController.js
+++ admin/controllers/UserController.js
@@ -1,30 +1,72 @@
  var express = require('express');
  var Passport = require('passport');
- var BasicStrategy = require('passport-http').BasicStrategy;
+ var LocalStrategy = require('passport-local').Strategy;
  var UserController = express();
  var User = require('../models/users.js');
 
 // passport configuration 
 
-Passport.use(new BasicStrategy(
+
+UserController.use(Passport.initialize());
+UserController.use(Passport.session());
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+Passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+Passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+
+Passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { 
-          console.log("Inside 1st Error block");
-          return done(err);
-      }
-      if (!user) {
-          console.log("Inside 2nd Error when it's not user");
-          return done(null, false); 
-      }
-      if (!user.verifyPassword(password)) { 
-          console.log("Inside 3rd Error VerifyPassword");
-          return done(null, false); 
-      }
-      return done(null, user);
-    });
+    findByUsername(username, function(err, user) {
+        if (err) { 
+          console.log('Error Occured During Authentication!!!');
+          return done(err); 
+        }
+        if (!user) { 
+          console.log('Unknown User!!!');
+          return done(null, false, { message: 'Unknown user ' + username }); 
+        }
+        if (user.password != password) { 
+          console.log('Wrong password!!!');
+          return done(null, false, { message: 'Invalid password' }); 
+        }
+        console.log('Authentication Successful ...'+user);
+        return done(null, user);
+   });
   }
 ));
+ 
 
 UserController.get("/login",function(req,res){
 	res.render("login");
@@ -35,20 +77,19 @@ UserController.get("/login",function(req,res){
 //UserController.use(Passport.initialize());
 //UserController.use(Passport.session());
 
-UserController.get('/authenticate',function(req,res){
+UserController.get('/authenticate',Passport.authenticate('local'),function(req,res){
+  console.log('Passport Authentication Done!!!');
+  res.json({msg:'Authentication Successful'});
+
+});
+
+/*UserController.get('/authenticate',function(req,res){
    console.log("Inside Authentication!!!!");
-   //res.render("User authenticated Successfully!!!");
-    //res.json({ msg:"User authenticated" });
-    Passport.authenticate('basic',function(req,res){
+    Passport.authenticate('local',function(req,res){
         res.json({msg: "authentication params" });
     });
 });
-                    
-                    
-//	Passport.authenticate('local',{successRedirect:'/',failureRedirect:'/login'}),
-//	function(req,res){
-//		res.json({ id: req.user.id, username: req.user.username });
-//	});
+*/
 
  module.exports = UserController;
 
