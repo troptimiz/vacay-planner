1354831eb02d86d1a1e3b543603635569623b2e1
satish.chandra <satish.chandra@valtech.co.in>
2016-03-03 00:32:32 +0530

    Date format changed

--- admin/public/js/bookings.js
+++ admin/public/js/bookings.js
@@ -75,4 +75,29 @@ $(document).ready(function(){
             }
         });
     });
+	
+	$( "#from" ).on('change', function () {
+		tableObject.dataTable().fnFilter( $(this).val() );
+	});
+	
+	$( "#to" ).on('change', function () {
+		tableObject.dataTable().fnFilter( $(this).val() );
+	});
+	
+	$( "#from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function(selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
 });
